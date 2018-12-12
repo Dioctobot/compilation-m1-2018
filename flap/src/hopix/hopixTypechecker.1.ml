11bef4c7e52e818dd7921b8a14117a9aaeac4120
rgv26 <rgv26.warforce@hotmail.fr>
2018-12-12 19:11:31 +0100

    typechecker en route de réussites

--- flap/src/hopix/hopixTypechecker.1.ml
+++ flap/src/hopix/hopixTypechecker.1.ml
@@ -33,72 +33,97 @@ let binop = let open HopixTypes in
   "`/`",          [hint; hint] --> hint;
 ]
 
-let rec check_definition_is_fully_annotated position = function
+let [@warning "-10"] rec check_definition_is_fully_annotated position = function
   | DefineValue vd -> 
-    Printf.printf "%s\n" HopixPrettyPrinter.(to_string (value_definition false) vd);
-    check_value_definition_is_fully_annotated position vd
+    let [@warning "-10"] aux = check_value_definition_is_fully_annotated position vd in 
+    aux;
+    ()
   | _ -> ()
 
-
-and check_expression_is_fully_annotated position = function
-  | Literal lit -> located check_literal_is_fully_annotated lit
-  | Variable (id, lty) -> 
-    begin match lty with
-      | None -> Printf.printf "None\n"
-      | Some t ->  Printf.printf "var lty = %d\n" (List.length t)
-    end;
-    located check_apply_identifier id
+and check_expression_is_fully_annotated x ty pos = function
+  | Literal lit -> (x, located check_literal_is_fully_annotated lit)
+  | Variable (id, _) -> located (check_variable_is_fully_annotated x ty) id
   | Define (vd, expr) -> 
-    Printf.printf "local def= %s\n" HopixPrettyPrinter.(to_string (value_definition true) vd);
-    Printf.printf "expr= %s\n" HopixPrettyPrinter.(to_string expression expr.value);
-    located check expr
+    let ch = check_value_definition_is_fully_annotated pos vd in
+    located (check (fst ch) (snd ch)) expr
   | Apply (expr, lexpr) ->
     let rec aux l = function
-          | [] -> (List.rev l)
-          | hd::tl -> aux ((located check hd)::l) tl
+      | [] -> (List.rev l)
+      | hd::tl -> aux (snd (located (check x ty) hd)::l) tl
     in
-    begin match located check expr with
-      | ATyArrow (tys, ty) -> 
-        if tys = (aux [] lexpr) then
-          ty
-        else
-          type_error expr.position "Apply : Not fully annoted"
-      | _ -> type_error expr.position "Apply : Wrong type"
-    end
-  | Field (expr, _) | Ref expr | Read expr ->
-    located check expr
-  | _ -> type_error position "TODO"
+    let (id, at) = located (check x ty) expr in
+    if [at] = (aux [] lexpr) then
+      (id, at)
+    else
+      begin match at with
+        | ATyArrow (tys, ty) -> let lty = aux [] lexpr in
+          if tys = lty then
+            (id, ty)
+          else
+            type_error expr.position "Apply : Not fully annoted"
+        | _ -> type_error expr.position "Apply : Wrong type"
+      end
+  | _ -> type_error pos "TODO"
 
-and check position = check_expression_is_fully_annotated position
+and check x ty pos = check_expression_is_fully_annotated x ty pos
+
+
+and [@warning "-10"] check_function_definition_is_fully_annotated x t pos = function
+  | FunctionDefinition (lid, expr) ->
+    let aux = List.map (fun id -> 
+      located (check_expression_is_fully_annotated id t) expr) 
+      (List.map (Position.value) lid)
+    in
+    (x, t)
 
 and check_value_definition_is_fully_annotated position = function
   | SimpleValue (id, tscheme, expr) -> 
     begin match tscheme with
       | None -> type_error id.position "SimpleValue : expected type scheme"
       | Some scheme ->
-      let open HopixTypes in
-      let typ = ty_of_scheme scheme.value in
-      let t = located check_expression_is_fully_annotated expr in
-      if aty_of_ty typ = t then
-        ()
-      else
-        type_error id.position "SimpleValue : not fully"
+        let x = id.value in
+        let typ = aty_of_ty (ty_of_scheme scheme.value) in
+        let (new_x, aty) = located (check_expression_is_fully_annotated x typ) expr in
+        if typ = aty then
+          (x, typ)
+        else
+          type_error id.position "SimpleValue is not fully annotated"
     end
-  | RecFunctions _ -> type_error position "RecFunctions"
-
+  | RecFunctions lfd ->
+    let aux = List.map (fun (id, tscheme, fd) -> 
+      let pos = (Position.position id) in
+        begin match tscheme with
+          | None -> type_error pos "RecFunctions : expected type scheme"
+          | Some scheme ->
+            let x = (Position.value id) in
+            let typ = aty_of_ty (ty_of_scheme (Position.value scheme)) in
+            if (x, typ) = (check_function_definition_is_fully_annotated x typ pos fd) then
+              (x, typ)
+            else
+              type_error pos "RecFunctions is not fully annotated"
+        end) lfd
+    in
+    List.iter (fun (id, t) -> Printf.printf "rec ty = %s\n" HopixTypes.(print_aty t)) aux;
+    List.hd aux
+    
+  
 and ty_of_scheme = function
   | ForallTy (_, t) -> 
     t.value
 
 and check_literal_is_fully_annotated position = function
-  | LInt _ -> hint
-  | LString _ -> hstring
-  | LChar _ -> hchar
+  | LInt _ -> HopixTypes.hint
+  | LString _ -> HopixTypes.hstring
+  | LChar _ -> HopixTypes.hchar
 
-and check_apply_identifier position = function
-  | _ as id -> 
-  try List.assoc id binop
-  with Not_found -> type_error position "check_apply_identifier"
+and check_variable_is_fully_annotated x ty pos = function
+  | _ as id ->
+  try id, List.assoc id binop
+  with Not_found -> 
+    if id = x then
+      (id, ty)
+    else
+      (x, ty)
 
 (** [check_program_is_fully_annotated ast] performs a syntactic check
  that the programmer wrote sufficient type annotations for [typecheck]
@@ -129,7 +154,7 @@ and check_definition tenv = function
     let l = List.map Position.value ltvar in
     let env = typecheck_type_definition tcons.value l tenv td in
     bind_type_variables tcons.position env l
-  | DeclareExtern (id, scheme) -> typecheck_extern_definition id.value (aty_scheme_of_type_scheme scheme.value) tenv
+  | DeclareExtern (id, scheme) -> typecheck_extern_definition id.value (aty_scheme_of_type_scheme tenv scheme.value) tenv
   | DefineValue vd -> typecheck_value_definition tenv vd
 
 and typecheck_type_definition x ts env = function
@@ -142,42 +167,118 @@ and typecheck_type_definition x ts env = function
 
 and typecheck_extern_definition x scheme env = bind_value x scheme env
 
+(*Printf.printf "x = %s\n"  
+      HopixPrettyPrinter.(to_string identifier x);
+      Printf.printf "expr = %s\n" 
+      HopixPrettyPrinter.(to_string expression expr.value);
+      Printf.printf "id = %s\n"  
+      HopixPrettyPrinter.(to_string identifier id);
+    Printf.printf "aty = %s\n"
+        HopixTypes.(print_aty aty);*)
+
 and typecheck_value_definition env = function
-  | SimpleValue (id, _, expr) ->
-    let (e, t) = typecheck_expression' env expr in
-    bind_value id.value t e
-  | _ -> env
+  | SimpleValue (id, tscheme, expr) -> let x = id.value in
+    begin match tscheme with
+      | None -> failwith "Should not be reached"
+      | Some scheme -> 
+        let e = bind_value id.value (aty_scheme_of_type_scheme env scheme.value) env in
+        fst (typecheck_expression' x e expr)
+    end
+  | RecFunctions lfd ->
+    let rec aux e = function
+      | [] -> e
+      | (id, o, fd)::tl ->
+        begin match o with
+          | None -> failwith "Should not be reached"
+          | Some scheme -> 
+            let x = Position.value id in
+            let t = aty_scheme_of_type_scheme e (Position.value scheme) in
+            let ev = bind_value x t e in
+            
+            aux (typecheck_function_definition x t ev fd) tl
+        end
+    in aux env lfd
 
-and typecheck_expression' env e =
-  (*Printf.printf "expr = %s\n" HopixPrettyPrinter.(to_string expression e.value);*)
-  located (typecheck_expression env) e
+and typecheck_function_definition x aty env = function
+  | FunctionDefinition (lid, expr) ->
+    let tys = list_of_aty (type_of_type_scheme aty) in
+    Printf.printf "expr = %s\n" 
+      HopixPrettyPrinter.(to_string expression expr.value);
+    print_endline (print_typing_environment env);
+    Printf.printf "aty = %s\n"
+          HopixTypes.(print_aty (type_of_type_scheme aty));
+    List.iter (fun at -> 
+    Printf.printf "at = %s\n" 
+      HopixTypes.(print_aty at))
+    tys;
+    let (ev, t) = typecheck_expression' x env expr in
+    
+    let rec aux e = function
+      | [] -> e
+      | hd::tl ->
+        let (new_e, new_t) = typecheck_expression' hd e expr in
+        Printf.printf "new aty = %s\n"
+              HopixTypes.(print_aty (type_of_type_scheme new_t));
+        
+      aux e tl
+    in 
+    aux env (List.map (Position.value) lid)
 
-and typecheck_expression env pos = function
+and typecheck_expression' x env e =
+  located (typecheck_expression x env) e
+
+and typecheck_expression x env pos = function
   | Literal lit -> 
-    let aty_scheme = mk_type_scheme (type_check_literal lit.value) in
+    let aty_scheme = monotype (type_check_literal lit.value) in
     env, aty_scheme
-  | Variable (id, _) ->
-    let aty_scheme = mk_type_scheme (located check_apply_identifier id) in
-    let e = bind_value id.value aty_scheme env in
-    e, aty_scheme
+  | Variable (id, _) -> let x = Position.value id in
+    begin
+      try 
+        let aty_scheme = mk_type_scheme (List.assoc x binop) in
+        let e = bind_value x aty_scheme env in
+        e, aty_scheme
+      with Not_found -> 
+        Printf.printf "x = %s\n"  
+          HopixPrettyPrinter.(to_string identifier x);
+        let aty_scheme = lookup_type_scheme_of_value pos x env in
+        env, aty_scheme
+    end
+  | Define (vd, expr) ->
+    let e = typecheck_value_definition env vd in
+    typecheck_expression' x e expr
   | Apply (expr, lexpr) ->
-    let (e, aty) = typecheck_expression' env expr in
+    let (e, aty) = typecheck_expression' x env expr in
     let rec aux ev = function
       | [] -> failwith "Should not be reached"
-      | [hd] -> typecheck_expression' ev hd
+      | [hd] -> typecheck_expression' x ev hd
       | hd::tl ->
-        let (new_e, aty) = typecheck_expression' ev hd in
+        let (new_e, aty) = typecheck_expression' x ev hd in
         aux new_e tl
     in aux e lexpr
   | _ -> env, mk_type_scheme HopixTypes.hunit
 
 
-and aty_scheme_of_type_scheme = function
-  | ForallTy (ltvar, ty) -> HopixTypes.(Scheme ((List.map Position.value ltvar), aty_of_ty ty.value))
+and aty_scheme_of_type_scheme env = function
+  | ForallTy (ltvar, ty) -> HopixTypes.(Scheme ((List.map Position.value ltvar), internalize_ty env ty))
+
+and type_of_type_scheme = function
+  | HopixTypes.Scheme (_, ty) -> ty
+
+and type_variables_of_type_scheme = function
+  | HopixTypes.Scheme (ltvar, _) -> ltvar
 
 and type_check_literal = function
   | LInt _ -> HopixTypes.hint
   | LString _ -> HopixTypes.hstring
   | LChar _ -> HopixTypes.hchar
+
+and type_check_variable env pos = function
+  | _ as id ->
+  try mk_type_scheme (List.assoc id binop)
+  with Not_found -> lookup_type_scheme_of_value pos id env
+
+and list_of_aty = function
+  | ATyArrow (tys, ty) -> tys
+  | _ -> failwith "Expected type : ATyArrow"
 
 let print_typing_environment = HopixTypes.print_typing_environment
--- flap/src/hopix/hopixTypechecker.ml
+++ flap/src/hopix/hopixTypechecker.ml
@@ -10,7 +10,7 @@ type typing_environment = HopixTypes.typing_environment
 let type_error = HopixTypes.type_error
 
 let located f x = f (Position.position x) (Position.value x)
-    (*Printf.printf "%s\n" HopixPrettyPrinter.(to_string optional_type_scheme_annotation otsch);*)
+
 let binop = let open HopixTypes in
   List.map (fun (x, s) -> Id x, s)
 [
@@ -33,59 +33,78 @@ let binop = let open HopixTypes in
   "`/`",          [hint; hint] --> hint;
 ]
 
+let aty_of_literal position = function
+  | LInt _ -> hint
+  | LString _ -> hstring
+  | LChar _ -> hchar
+
 let rec check_definition_is_fully_annotated position = function
   | DefineValue vd -> check_value_definition_is_fully_annotated position vd
   | _ -> ()
 
-and check_expression_is_fully_annotated x t pos = function
-  | Literal lit ->( x, located check_literal_is_fully_annotated lit)
-  | _ -> type_error pos "TODO"
+and check_expression_is_fully_annotated' x aty e = 
+  located (check_expression_is_fully_annotated x aty) e
 
-and check position = check_expression_is_fully_annotated position
+and check_expression_is_fully_annotated x aty pos = function
+  | Literal lit -> (x, located aty_of_literal lit)
+  | Variable (id, _) -> 
+    located (check_variable_is_fully_annotated x aty) id
+  | Apply (expr, lexpr) ->
+    let rec aux l = function
+      | [] -> (List.rev l)
+      | hd::tl -> aux (snd (located (check x aty) hd)::l) tl
+    in
+    let (id, at) = located (check x aty) expr in
+    
+    if [at] = (aux [] lexpr) then
+      (id, at)
+    else
+      begin match at with
+        | ATyArrow (tys, ty) -> let lty = aux [] lexpr in
+          if tys = lty then
+            (x, ty)
+          else
+            type_error expr.position "Apply : Not fully annoted"
+        | _ -> type_error expr.position "Apply : Wrong type"
+      end
+  | _ -> type_error pos "check_expression_is_fully_annotated : non-exhaustive pattern matching"
 
-and check_function_definition_is_fully_annotated x t pos = function
-  | FunctionDefinition (lid, expr) -> (x, t)
+and check x ty pos = check_expression_is_fully_annotated x ty pos
 
 and check_value_definition_is_fully_annotated position = function
-  | SimpleValue (id, tscheme, expr) -> 
+  | SimpleValue (id, tscheme, expr) ->
+    let (pos, x) = Position.position id, Position.value id in
     begin match tscheme with
-      | None -> type_error id.position "SimpleValue : expected type scheme"
+      | None -> ()
       | Some scheme ->
-        let x = id.value in
-        let typ = aty_of_ty (ty_of_scheme scheme.value) in
-        if (x, typ) = check_expression_is_fully_annotated x typ expr.position expr.value then
+        let type_scheme = Position.value scheme in
+        let aty = aty_of_ty' (ty_of_type_scheme type_scheme) in
+        if (x, aty) = check_expression_is_fully_annotated' x aty expr then
           ()
         else
-          type_error id.position "SimpleValue : not fully"
+          type_error pos "SimpleValue is not fully annotated"
     end
   | RecFunctions lfd ->
-    let [@warning "-10"] aux = List.map (fun (id, tscheme, fd) -> let pos = (Position.position id) in
-    begin match tscheme with
-      | None -> type_error pos "RecFunctions : expected type scheme"
-      | Some scheme ->
-        let x = (Position.value id) in
-        let typ = aty_of_ty (ty_of_scheme (Position.value scheme)) in
-        if (x, typ) =  (check_function_definition_is_fully_annotated x typ pos fd) then
-          ()
-        else
-          type_error pos "RecFunctions : not fully"
-    end) lfd; ()
-    in aux
-    
-  
-and ty_of_scheme = function
-  | ForallTy (_, t) -> 
-    t.value
+    List.iter (fun (id, tscheme, fd) ->
+      let (pos, x) = Position.position id, Position.value id in
+      check_function_definition_is_fully_annotated x pos tscheme fd
+    ) lfd
 
-and check_literal_is_fully_annotated position = function
-  | LInt _ -> HopixTypes.hint
-  | LString _ -> HopixTypes.hstring
-  | LChar _ -> HopixTypes.hchar
+and check_function_definition_is_fully_annotated x position type_scheme = function
+  | FunctionDefinition (lid, expr) -> 
+    begin match type_scheme with
+      | None -> ()
+      | Some scheme -> assert false
+    end
 
-and check_apply_identifier position = function
-  | _ as id -> 
-  try List.assoc id binop
-  with Not_found -> type_error position "check_apply_identifier"
+and ty_of_type_scheme = function
+  | ForallTy (_, ty) -> ty
+
+and check_variable_is_fully_annotated x aty pos = function
+  | _ as id ->
+  try id, List.assoc id binop
+  with Not_found -> 
+    type_error pos "check_variable_is_fully_annotated is not fully annotated"
 
 (** [check_program_is_fully_annotated ast] performs a syntactic check
  that the programmer wrote sufficient type annotations for [typecheck]
@@ -93,30 +112,27 @@ and check_apply_identifier position = function
 let check_program_is_fully_annotated ast =
   List.iter (fun def -> located check_definition_is_fully_annotated def) ast
 
-exception FailureTypecheck of Position.position * string
-(*List.iter (fun (id, aty) -> Printf.printf "id = %s\n" (string_of_identifier id)) tenv.values*)
 (** [typecheck tenv ast] checks that [ast] is a well-formed program
     under the typing environment [tenv]. *)
 let rec typecheck tenv ast : typing_environment =
-  (*Printf.printf "%d\n" (List.length ast);*)
-  Printf.printf "PROGRAM :\n%s\nENDPROGRAM\n" HopixPrettyPrinter.(to_string program ast);
   check_program_is_fully_annotated ast;
-  let open HopixTypes in
-  let env = check_program tenv ast in
-  print_endline (print_typing_environment env);
+  let env = typecheck_program tenv ast in
+  (*Printf.printf "%s\n" (print_typing_environment env);*)
   env
 
-and check_program tenv = function
+and typecheck_program tenv = function
   | [] -> tenv
   | hd::tl -> 
-    check_program (check_definition tenv (Position.value hd)) tl
+    typecheck_program (typecheck_definition tenv (Position.value hd)) tl
 
-and check_definition tenv = function
+and typecheck_definition tenv = function
   | DefineType (tcons, ltvar, td) ->
     let l = List.map Position.value ltvar in
     let env = typecheck_type_definition tcons.value l tenv td in
     bind_type_variables tcons.position env l
-  | DeclareExtern (id, scheme) -> typecheck_extern_definition id.value (aty_scheme_of_type_scheme scheme.value) tenv
+  | DeclareExtern (id, scheme) ->
+    let (env, aty_scheme) = located (typecheck_type_scheme tenv) scheme in
+    typecheck_extern_definition id.value aty_scheme env
   | DefineValue vd -> typecheck_value_definition tenv vd
 
 and typecheck_type_definition x ts env = function
@@ -130,41 +146,53 @@ and typecheck_type_definition x ts env = function
 and typecheck_extern_definition x scheme env = bind_value x scheme env
 
 and typecheck_value_definition env = function
-  | SimpleValue (id, _, expr) ->
-    let (e, t) = typecheck_expression' env expr in
-    bind_value id.value t e
-  | _ -> env
+  | SimpleValue (id, tscheme, expr) ->
+    let (pos, x) = Position.position id, Position.value id in
+    begin match tscheme with
+      | None -> fst (typecheck_expression' x env expr)
+      | Some scheme ->
+        let (ev, aty_scheme) = located (typecheck_type_scheme env) scheme in
+        let new_env = bind_value x aty_scheme env in
+        fst (typecheck_expression' x new_env expr)
+    end
+  | RecFunctions lfd -> assert false
+    (*List.iter (fun (id, tscheme, fd) ->
+      let (pos, x) = Position.position id, Position.value id in
+      check_function_definition_is_fully_annotated x pos tscheme fd
+    ) lfd*)
 
-and typecheck_expression' env e =
-  (*Printf.printf "expr = %s\n" HopixPrettyPrinter.(to_string expression e.value);*)
-  located (typecheck_expression env) e
+and typecheck_expression' x env e =
+  located (typecheck_expression x env) e
 
-and typecheck_expression env pos = function
+and typecheck_expression x env pos = function
   | Literal lit -> 
-    let aty_scheme = mk_type_scheme (type_check_literal lit.value) in
+    let aty_scheme = monotype (located aty_of_literal lit) in
     env, aty_scheme
-  | Variable (id, _) ->
-    let aty_scheme = mk_type_scheme (located check_apply_identifier id) in
-    let e = bind_value id.value aty_scheme env in
-    e, aty_scheme
+  | Variable (id, _) -> let x = Position.value id in
+    begin
+      try 
+        let aty_scheme = mk_type_scheme (List.assoc x binop) in
+        let e = bind_value x aty_scheme env in
+        e, aty_scheme
+      with Not_found -> 
+        let aty_scheme = lookup_type_scheme_of_value pos x env in
+        env, aty_scheme
+    end
   | Apply (expr, lexpr) ->
-    let (e, aty) = typecheck_expression' env expr in
+    let (e, aty) = typecheck_expression' x env expr in
     let rec aux ev = function
       | [] -> failwith "Should not be reached"
-      | [hd] -> typecheck_expression' ev hd
+      | [hd] -> typecheck_expression' x ev hd
       | hd::tl ->
-        let (new_e, aty) = typecheck_expression' ev hd in
+        let (new_e, aty) = typecheck_expression' x ev hd in
         aux new_e tl
     in aux e lexpr
-  | _ -> env, mk_type_scheme HopixTypes.hunit
+  | _ -> env, monotype HopixTypes.hunit
 
-
-and aty_scheme_of_type_scheme = function
-  | ForallTy (ltvar, ty) -> HopixTypes.(Scheme ((List.map Position.value ltvar), aty_of_ty ty.value))
-
-and type_check_literal = function
-  | LInt _ -> HopixTypes.hint
-  | LString _ -> HopixTypes.hstring
-  | LChar _ -> HopixTypes.hchar
+and typecheck_type_scheme env pos = function
+  | ForallTy (ltvar, ty) -> 
+    let ts = List.map Position.value ltvar in
+    let e = bind_type_variables pos env ts in
+    e, Scheme (ts, internalize_ty e ty)
 
 let print_typing_environment = HopixTypes.print_typing_environment

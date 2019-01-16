d4a9f03cb109c28e9c7e7ad74b8b926a88f5aeb2
Yann Regis-Gianas <yrg@irif.fr>
2019-01-16 11:06:59 +0100

    Simplify Hello World.

--- doc/td-x86/code/exo-1.s
+++ doc/td-x86/code/exo-1.s
@@ -1,21 +1,12 @@
-	.file	"hello.c"
-	.text
-	.section	.rodata
-.LC0:
-	.string	"Hello World!"
 	.text
 	.globl	main
-	.type	main, @function
 main:
-	pushq	%rbp
-	movq	%rsp, %rbp
-	subq	$16, %rsp
-	movl	%edi, -4(%rbp)
-	movq	%rsi, -16(%rbp)
-	movl	$.LC0, %edi
+        mov     $message, %rdi 
 	call	puts
-	movl	$0, %edi
-	call	exit
-	.size	main, .-main
-	.ident	"GCC: (Debian 8.2.0-13) 8.2.0"
-	.section	.note.GNU-stack,"",@progbits
+	movq	$0, %rax
+        ret
+	.data
+	
+message:.string	"Hello World!"	
+
+	
